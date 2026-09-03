4880c7fc91cc3a3383aeaf954e1f5918df6ba13f
Viktoriia Pavlushka <146458258+ViktoriiaPavlushka@users.noreply.github.com>
2026-09-03 20:10:28 +0300

    Refactor safety_node.py
     Improved clarity in safety checks.

--- my_diff_robot/scripts/safety_node.py
+++ my_diff_robot/scripts/safety_node.py
@@ -11,9 +11,7 @@ class SafetyNode(Node):
         self.safe_front = True
         self.safe_rear = True
         
-        # Габарити з URDF (box size y=0.2 -> половина ширини 0.1 м)
         self.robot_half_width = 0.10
-        # Додаємо 5 см з кожного боку, щоб не зачепити кутами
         self.lateral_margin = 0.05 
 
         self.scan_sub = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
@@ -25,27 +23,21 @@ class SafetyNode(Node):
         danger_rear = False
 
         for i, r in enumerate(msg.ranges):
-            # Відкидаємо невалідні значення сенсора
             if math.isinf(r) or math.isnan(r) or r < msg.range_min or r > msg.range_max:
                 continue
 
-            # Знаходимо кут конкретного променя
             angle = msg.angle_min + i * msg.angle_increment
-            
-            # Конвертуємо полярні координати (r, кут) у декартові (x, y) відносно центру робота
+
             x = r * math.cos(angle)
             y = r * math.sin(angle)
 
-            # Перевіряємо, чи перешкода знаходиться в нашому коридорі по ширині
             if abs(y) <= (self.robot_half_width + self.lateral_margin):
-                
-                # Відстежуємо перешкоди на відстані до 0.5 м спереду та ззаду[cite: 1]
+     
                 if 0 < x <= 0.5:
                     danger_front = True
                 elif -0.5 <= x < 0:
                     danger_rear = True
 
-        # Екстрене гальмування при появі нової загрози
         if danger_front and self.safe_front:
             self.get_logger().warn('FRONT collision predicted! Stopping.')
             self.force_stop()
@@ -69,7 +61,6 @@ class SafetyNode(Node):
         safe_msg.header.stamp = self.get_clock().now().to_msg()
         safe_msg.twist = msg.twist
 
-        # Блокуємо команди керування відповідно до прорахованого коридору
         if safe_msg.twist.linear.x > 0.0 and not self.safe_front:
             safe_msg.twist.linear.x = 0.0
         elif safe_msg.twist.linear.x < 0.0 and not self.safe_rear:
